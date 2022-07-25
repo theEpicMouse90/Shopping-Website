6e8562cea00dcf82d78864c02a8922795892c994
theEpicMouse90 <68754253+theEpicMouse90@users.noreply.github.com>
2022-07-25 13:42:58 -0500

    changin things

--- server.py
+++ server.py
@@ -3,6 +3,10 @@ import os
 
 app = Flask(__name__)
 
+@app.route("/")
+def main():
+    return render_template("index.html")
+
 @app.route("/<string:Site>")
 def site(Site):
 	return render_template(Site)
